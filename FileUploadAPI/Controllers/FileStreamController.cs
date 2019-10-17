21cd63caecf7ff3f808f2f242b034b756294986a
Hitendra <hitendra.kumar1@tesco.com>
2019-10-17 12:44:26 +0530

    adding project files

---
+++ FileUploadAPI/Controllers/FileStreamController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using Microsoft.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using FileUploadAPI.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace FileUploadAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class FileStreamController : ControllerBase
+    {
+        private readonly long _fileSizeLimit;
+        private readonly ILogger<FileStreamController> _logger;
+        private readonly string[] _permittedExtensions = { ".txt",".pdf",".csv" };
+        private readonly string _targetFilePath;
+        private static readonly FormOptions _defaultFormOptions = new FormOptions();
+
+        public FileStreamController(ILogger<FileStreamController> logger,IConfiguration config)
+        {
+            _logger = logger;
+            _fileSizeLimit = config.GetValue<long>("FileSizeLimit");
+
+            // To save physical files to a path provided by configuration:
+            _targetFilePath = config.GetValue<string>("StoredFilesPath");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            var trustedFileNameForFileStorage = string.Empty;
+            if (!MultipartRequestHelper.IsMultipartContentType(Request.ContentType))
+            {
+                ModelState.AddModelError("File",
+                    $"The request couldn't be processed (Error 1).");
+                _logger.LogError("The request couldn't be processed (Error 1).");
+
+                return BadRequest(ModelState);
+            }
+
+            var boundary = MultipartRequestHelper.GetBoundary(
+                MediaTypeHeaderValue.Parse(Request.ContentType),
+                _defaultFormOptions.MultipartBoundaryLengthLimit);
+            var reader = new MultipartReader(boundary, HttpContext.Request.Body);
+            var section = await reader.ReadNextSectionAsync();
+
+            while (section != null)
+            {
+                var hasContentDispositionHeader =
+                    ContentDispositionHeaderValue.TryParse(
+                        section.ContentDisposition, out var contentDisposition);
+
+                if (hasContentDispositionHeader)
+                {
+                    // This check assumes that there's a file
+                    // present without form data. If form data
+                    // is present, this method immediately fails
+                    // and returns the model error.
+                    if (!MultipartRequestHelper
+                        .HasFileContentDisposition(contentDisposition))
+                    {
+                        ModelState.AddModelError("File",
+                            $"The request couldn't be processed (Error 2).");
+                        // Log error
+                        _logger.LogError("The request couldn't be processed (Error 2).");
+
+                        return BadRequest(ModelState);
+                    }
+                    else
+                    {
+                        // Don't trust the file name sent by the client. To display
+                        // the file name, HTML-encode the value.
+                        var trustedFileNameForDisplay = WebUtility.HtmlEncode(
+                                contentDisposition.FileName.Value);
+                        // var trustedFileNameForFileStorage = Path.GetRandomFileName();
+
+                        trustedFileNameForFileStorage = trustedFileNameForDisplay;
+
+                        // **WARNING!**
+                        // In the following example, the file is saved without
+                        // scanning the file's contents. In most production
+                        // scenarios, an anti-virus/anti-malware scanner API
+                        // is used on the file before making the file available
+                        // for download or for use by other systems. 
+                        // For more information, see the topic that accompanies 
+                        // this sample.
+
+                        var streamedFileContent = await FileHelpers.ProcessStreamedFile(
+                            section, contentDisposition, ModelState,
+                            _permittedExtensions, _fileSizeLimit);
+
+                        if (!ModelState.IsValid)
+                        {
+                            return BadRequest(ModelState);
+                        }
+
+                        using (var targetStream = System.IO.File.Create(
+                            Path.Combine(_targetFilePath, trustedFileNameForFileStorage)))
+                        {
+                            await targetStream.WriteAsync(streamedFileContent);
+                            if(!VirusScan(Path.Combine(_targetFilePath, trustedFileNameForFileStorage)))
+                            {
+                                _logger.LogInformation($"file {_targetFilePath} not uploaded to server contains virus.");
+                                return BadRequest(new { Message = "file not uploaded to server contains virus." });
+                            }
+                            _logger.LogInformation(
+                                "Uploaded file '{TrustedFileNameForDisplay}' saved to " +
+                                "'{TargetFilePath}' as {TrustedFileNameForFileStorage}",
+                                trustedFileNameForDisplay, _targetFilePath,
+                                trustedFileNameForFileStorage);
+                        }
+                    }
+                }
+
+                // Drain any remaining section body that hasn't been consumed and
+                // read the headers for the next section.
+                section = await reader.ReadNextSectionAsync();
+            }
+
+            return Created(nameof(FileStreamController), new {Message=$"File {trustedFileNameForFileStorage} uploaded successfully" });
+        }
+
+
+        private static Encoding GetEncoding(MultipartSection section)
+        {
+            var hasMediaTypeHeader =
+                MediaTypeHeaderValue.TryParse(section.ContentType, out var mediaType);
+
+            // UTF-7 is insecure and shouldn't be honored. UTF-8 succeeds in 
+            // most cases.
+            if (!hasMediaTypeHeader || Encoding.UTF7.Equals(mediaType.Encoding))
+            {
+                return Encoding.UTF8;
+            }
+
+            return mediaType.Encoding;
+        }
+
+
+        private static bool VirusScan(string filePath)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "C:\\Program Files\\Windows Defender\\MpCmdRun.exe";
+            startInfo.Arguments = "-Scan -ScanType 3 -File " + filePath;
+            process.StartInfo = startInfo;
+            var result = process.Start();
+            if (!result)
+            {
+                FileInfo fileInfo = new FileInfo(filePath);
+                fileInfo.Delete();
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
---
+++ FileUploadAPIClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+
+namespace FileUploadAPIClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileTransfer();
+        }
+
+        static void FileTransfer()
+        {
+            FileInfo file = new FileInfo(@"C:\FileTest\CLI.pdf");
+            using (var client = new HttpClient())
+            {
+                using (var content = new MultipartFormDataContent())
+                {
+                    var fileStream = File.Open(@"C:\FileTest\CLI.pdf", FileMode.Open);
+                    content.Add(new StreamContent(fileStream), "\"file\"", string.Format("\"{0}\"", file.Name));
+                    //var requestUri = "https://localhost:44319/api/v1/filestream";
+                    var requestUri = "http://localhost:5000/api/v1/filestream";
+                    var result = client.PostAsync(requestUri, content).Result;
+                    if (result.StatusCode == System.Net.HttpStatusCode.Created)
+                    {
+                        var body = result.Content.ReadAsStringAsync().Result;
+                    }
+                }
+            }
+        }
+    }
+}
